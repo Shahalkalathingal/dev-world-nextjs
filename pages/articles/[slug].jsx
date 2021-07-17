b05d1cb117bbd9caa1ea2a62cbdf14179209ad23
ShahalKalathingal <shahal8002@gmail.com>
2021-07-17 17:09:19 +0530

    displayed data in article page with getStaticProps and getStaticPaths

--- pages/articles/[slug].jsx
+++ pages/articles/[slug].jsx
@@ -1,11 +1,51 @@
 import Layout from "@/components/Layout"
+import { API_URL } from "@/config/index"
 
-function ArticlePage() {
+function ArticlePage({ article }) {
     return (
         <Layout>
-            Event
+            <h1>{article.name}</h1>
         </Layout>
     )
 }
+
+
+
+export async function getStaticPaths() {
+    const res = await fetch(`${API_URL}/api/articles`)
+    const articles = await res.json()
+
+    const paths = articles.map((article) => ({
+        params: { slug: article.slug }
+    }))
+    return {
+        paths,
+        fallback: true
+    }
+}
+
+
+export async function getStaticProps({ params: { slug } }) {
+    const res = await fetch(`${API_URL}/api/articles/${slug}`)
+    const articles = await res.json()
+    return {
+        props: {
+            article: articles[0]
+        },
+        revalidate: 1
+    }
+}
+
+
+
+// export async function getServerSideProps({query:{slug}}){
+//     const res = await fetch(`${API_URL}/api/articles/${slug}`)
+//     const articles = await res.json()
+//     return{
+//         props:{
+//             article:articles[0]
+//         }
+//     }
+// }
 
 export default ArticlePage
